39fc6e8368a0f6973e5f10fb124d4a098833fc97
sahe <jackitehm@gmail.com>
2025-05-30 09:54:17 +0300

    meeting project requirements

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,10 +1,10 @@
 import { PillProps } from "@/interfaces";
-import React from "react";
+// import React from "react";
 
 const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">{ title }</p>
+      <p className=" text-sm ">{title}</p>
     </div>
   )
 }
